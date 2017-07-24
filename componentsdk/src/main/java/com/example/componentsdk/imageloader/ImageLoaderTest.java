3b2aff2575036246af425ddb91a5682bb81269a6
JD <623893416@qq.com>
2017-07-24 20:29:00 +0800

    提交图片加载的封装

---
+++ componentsdk/src/main/java/com/example/componentsdk/imageloader/ImageLoaderTest.java
@@ -0,0 +1,14 @@
+package com.example.componentsdk.imageloader;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+/**
+ * Created by JDNew on 2017/7/24.
+ */
+
+public class ImageLoaderTest {
+
+    private void testApi(){
+        ImageLoader imageLoader = ImageLoader.getInstance();
+    }
+}
